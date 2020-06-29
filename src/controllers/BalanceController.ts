eebef3238d362be7756f2f54aff901dbb03bcdfa
Stephani Lacerda <s.lacerda@outelook.com>
2020-06-29 13:16:27 -0300

    created balancecontroller.ts

---
+++ src/controllers/BalanceController.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import knex from '../database/connection';
+
+class BalanceController {
+    async create(request: Request, response: Response, next: NextFunction) {
+
+    }
+
+    async index(request: Request, response: Response, next: NextFunction) {
+
+    }
+
+    async show(request: Request, response: Response, next: NextFunction) {
+
+    }
+
+    async update(request: Request, response: Response, next: NextFunction) {
+
+    }
+
+    async delete(request: Request, response: Response, next: NextFunction) {
+        
+    }
+
+}
+
+export default BalanceController;
