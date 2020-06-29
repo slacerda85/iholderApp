10f50b0d0452441f71174a4e26a46e0f6c435a08
Stephani Lacerda <s.lacerda@outelook.com>
2020-06-29 12:48:07 -0300

    changed balance migration

--- src/database/migrations/03_create_balance.ts
+++ src/database/migrations/03_create_balance.ts
@@ -2,12 +2,12 @@ import Knex from 'knex';
 
 export async function up(knex: Knex) {
     return knex.schema.createTable('balance', table => {
-        table.increments('id').primary();
         table.string('asset_ticker')
         .notNullable()
         .references('ticker')
-        .inTable('assets');
+        .inTable('assets').primary();
         table.integer('qtd').notNullable();
+        table.float('avg_price').notNullable();
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.timestamp('updated_at').defaultTo(knex.fn.now());
     })
