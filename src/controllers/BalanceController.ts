296d2a497b5775ea05594303e905ba92d5e830dc
Stephani Lacerda <s.lacerda@outelook.com>
2020-06-29 13:29:57 -0300

    updated balancecontroller

--- src/controllers/BalanceController.ts
+++ src/controllers/BalanceController.ts
@@ -3,7 +3,24 @@ import knex from '../database/connection';
 
 class BalanceController {
     async create(request: Request, response: Response, next: NextFunction) {
+      try {
+        const {
+          asset_ticker,
+          qtd,
+          avg_price
+        } = request.body;
 
+        await knex('balance').insert({
+          asset_ticker,
+          qtd,
+          avg_price
+        });
+
+        return response.send();
+
+      } catch (error) {
+        next(error);
+      }
     }
 
     async index(request: Request, response: Response, next: NextFunction) {
@@ -19,7 +36,7 @@ class BalanceController {
     }
 
     async delete(request: Request, response: Response, next: NextFunction) {
-        
+
     }
 
 }
