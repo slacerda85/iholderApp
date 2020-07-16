0fbbc6f36b1a8d448626fa5490a686f7cbdfb487
Stephani Lacerda <s.lacerda@outelook.com>
2020-07-16 11:56:26 -0300

    save changes

--- src/controllers/PortfolioController.ts
+++ src/controllers/PortfolioController.ts
@@ -1,9 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 import knex from '../database/connection';
-import getLastPrice from '../services/getLastPrice';
+import formatPortfolio from '../services/formatPortfolio';
 
 interface Portfolio {
   asset_ticker: string,
+  description: string,
   qtd: number,
   avg_price: number,
   lastPrice: number,
@@ -12,6 +13,7 @@ interface Portfolio {
 }
 
 class PortfolioController {
+  
   async create(request: Request, response: Response, next: NextFunction) {
     try {
       const {
@@ -30,38 +32,17 @@ class PortfolioController {
   }
 
   async index(request: Request, response: Response, next: NextFunction) {
-
-    //Função que calcula e monta o objeto com os dados do ativo
-    async function mountPortfolio(portfolio: Portfolio[]) {
-
-      const result = portfolio.map(async (item: Portfolio) => {
-
-        const lastPrice = await getLastPrice(item.asset_ticker);
-
-        const percent = ((item.qtd * lastPrice - item.avg_price) / item.avg_price * 100);
-
-        console.log(percent);
-
-        return {
-          asset_ticker: item.asset_ticker,
-          qtd: item.qtd,
-          avg_price: item.avg_price,
-          lastPrice: (lastPrice != undefined ? lastPrice : 0),
-          profit: (item.qtd * lastPrice - item.avg_price),
-          percent: isNaN(percent) ? 0 : percent
-        };
-      });
-
-      const finalResult = await Promise.all(result);
-
-      return finalResult;
-    }
-
-    //função que busca o portfolio, envia como parametro para a função acima, e retorna o resultado.
+    
     try {
-      let portfolio: Portfolio[] = await knex('portfolio').select('*');
-
-      const result = await mountPortfolio(portfolio);
+      //Busca o portfolio no DB
+      const portfolio: Portfolio[] = await knex('portfolio').select('*');
+      
+      //mapeia os itens, e chama a função de formatação de dados para retorno.
+      const result = await Promise.all(portfolio.map(item => {
+        return (
+          formatPortfolio(item)
+        );
+      }));
 
       return response.json(result);
 
---
+++ src/services/formatPortfolio.ts
@@ -0,0 +1,35 @@
+import knex from '../database/connection';
+import getLastPrice from './getLastPrice';
+
+interface Portfolio {
+    asset_ticker: string,
+    description: string,
+    qtd: number,
+    avg_price: number,
+    lastPrice: number,
+    profit: number,
+    percent: number
+  }
+
+async function formatPortfolio(item: Portfolio) {
+
+    const data = await knex.column('description').from('assets').where({ticker: item.asset_ticker});
+    const description = data[0].description;
+    const lastPrice = await getLastPrice(item.asset_ticker);
+    const percent = ((item.qtd * lastPrice - item.avg_price) / item.avg_price * 100);
+
+    const teste = await Promise.all(description);
+    console.log(description);
+
+    return {
+      asset_ticker: item.asset_ticker,
+      description: description,
+      qtd: item.qtd,
+      avg_price: item.avg_price,
+      lastPrice: (lastPrice != undefined ? lastPrice : 0),
+      profit: (item.qtd * lastPrice - item.avg_price),
+      percent: isNaN(percent) ? 0 : percent
+    };
+}
+
+export default formatPortfolio;
