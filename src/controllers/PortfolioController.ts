d60d3bf948ce2dbf4ba7a604cfcf7d18c6152353
Stephani Lacerda <s.lacerda@outelook.com>
2020-07-16 09:46:16 -0300

    fixed promise problems

--- src/controllers/PortfolioController.ts
+++ src/controllers/PortfolioController.ts
@@ -1,5 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 import knex from '../database/connection';
+import getLastPrice from '../services/getLastPrice';
+
+interface Portfolio {
+  asset_ticker: string,
+  qtd: number,
+  avg_price: number,
+  lastPrice: number,
+  profit: number,
+  percent: number
+}
 
 class PortfolioController {
   async create(request: Request, response: Response, next: NextFunction) {
@@ -20,10 +30,40 @@ class PortfolioController {
   }
 
   async index(request: Request, response: Response, next: NextFunction) {
-    try {
-      const portfolio = await knex('portfolio').select('*');
 
-      return response.json(portfolio);
+    //Função que calcula e monta o objeto com os dados do ativo
+    async function mountPortfolio(portfolio: Portfolio[]) {
+
+      const result = portfolio.map(async (item: Portfolio) => {
+
+        const lastPrice = await getLastPrice(item.asset_ticker);
+
+        const percent = ((item.qtd * lastPrice - item.avg_price) / item.avg_price * 100);
+
+        console.log(percent);
+
+        return {
+          asset_ticker: item.asset_ticker,
+          qtd: item.qtd,
+          avg_price: item.avg_price,
+          lastPrice: (lastPrice != undefined ? lastPrice : 0),
+          profit: (item.qtd * lastPrice - item.avg_price),
+          percent: isNaN(percent) ? 0 : percent
+        };
+      });
+
+      const finalResult = await Promise.all(result);
+
+      return finalResult;
+    }
+
+    //função que busca o portfolio, envia como parametro para a função acima, e retorna o resultado.
+    try {
+      let portfolio: Portfolio[] = await knex('portfolio').select('*');
+
+      const result = await mountPortfolio(portfolio);
+
+      return response.json(result);
 
     } catch (error) {
       next(error);
@@ -33,15 +73,15 @@ class PortfolioController {
   async show(request: Request, response: Response, next: NextFunction) {
     try {
       const { asset_ticker } = request.params;
-      
-      const asset = await knex('portfolio')
-      .where({ asset_ticker: asset_ticker.toUpperCase() });
 
-    return response.json(asset);
+      const asset = await knex('portfolio')
+        .where({ asset_ticker: asset_ticker.toUpperCase() });
+
+      return response.json(asset);
     } catch (error) {
       next(error);
-    } 
-  }  
+    }
+  }
 
   async update(request: Request, response: Response, next: NextFunction) {
     try {
@@ -49,7 +89,7 @@ class PortfolioController {
         qtd,
         avg_price
       } = request.body;
-      const { asset_ticker } = request.params;      
+      const { asset_ticker } = request.params;
 
       await knex('portfolio').update({
         qtd: qtd,
---
+++ src/services/api.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://cotacao.b3.com.br/mds/api/v1/DailyFluctuationHistory/'
+  });
+
+export const tesourodireto = axios.create({
+    baseURL: 'https://www.tesourodireto.com.br/json/br/com/b3/tesourodireto/service/api/treasurybondsinfo.json'
+});
+
+export default api;
